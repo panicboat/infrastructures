9df0273f4aa34a58e6142e3f63f1d41972ca778e
panicboat <panicboat@gmail.com>
2021-10-01 23:19:31 +0900

    Add AuditStack

--- src/sandbox/lib/audit-stack.ts
+++ src/sandbox/lib/audit-stack.ts
@@ -1,15 +1,14 @@
 import * as cdk from '@aws-cdk/core';
-import { VpcResources } from 'cdk-common/vpc';
+import { AuditResources } from 'cdk-common/audit';
 
-export class SandboxStack extends cdk.Stack {
+export class AuditStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
     require('dotenv').config();
-    new VpcResources(this, id, {
+    new AuditResources(this, id, {
       projectName: process.env.PROJECT_NAME!,
-      cidrBlock: process.env.VPC_CIDR_BLOCK!,
     });
   }
 }
