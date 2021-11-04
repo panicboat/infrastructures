082aac5f32d188772e9ed900ea7e9e78903dbf9c
panicboat <panicboat@gmail.com>
2021-11-04 20:48:21 +0900

    Remove AuditStack

--- src/sandbox/bin/sandbox.ts
+++ src/sandbox/bin/sandbox.ts
@@ -2,13 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { SandboxStack } from '../lib/sandbox-stack';
-import { AuditStack } from '../lib/audit-stack';
 
 require('dotenv').config();
 const app = new cdk.App();
-new AuditStack(app, 'SandboxAuditStack', {
-  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.CDK_DEFAULT_REGION },
-});
 new SandboxStack(app, 'SandboxNetworkStack', {
   env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.CDK_DEFAULT_REGION },
 });
