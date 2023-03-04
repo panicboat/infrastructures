1548eb79c4ecee5a4cc7fec58163742341240169
panicboat <panicboat@gmail.com>
2023-03-04 13:38:45 +0900

    fix

--- src/sandbox/lib/sandbox-stack.ts
+++ src/sandbox/lib/sandbox-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+require('dotenv').config();
+
 export class SandboxStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
