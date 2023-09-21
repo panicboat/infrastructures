8b14be36b3c171ac28e416033ccf9313cb178cc9
panicboat <panicboat@gmail.com>
2023-09-21 10:43:59 +0900

    fix

--- src/sandbox/lib/sandbox-eks-stack.ts
+++ src/sandbox/lib/sandbox-eks-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as k8s from 'aws-cdk-modules/eks';
 import { Construct } from 'constructs';
 
 export interface Props extends cdk.StackProps {
@@ -10,12 +9,5 @@ export interface Props extends cdk.StackProps {
 export class SandboxEksStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
-
-    const k8sCluster = new k8s.Cluster(this, 'Cluster-Sandbox', {
-      name: 'sandbox',
-      endpointAccess: cdk.aws_eks.EndpointAccess.PUBLIC_AND_PRIVATE,
-      version: cdk.aws_eks.KubernetesVersion.V1_27,
-      vpc: props.vpc
-    });
   }
 }
