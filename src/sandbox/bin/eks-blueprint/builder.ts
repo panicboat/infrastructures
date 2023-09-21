b8fcb3fca1c2bf9c7dc92d7506f42875a304f91d
panicboat <panicboat@gmail.com>
2023-09-21 12:39:13 +0900

    fix

--- src/sandbox/bin/eks-blueprint/builder.ts
+++ src/sandbox/bin/eks-blueprint/builder.ts
@@ -1,14 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
 import { Construct } from 'constructs';
 
 interface IEksBuilder {
 }
 export interface Props extends cdk.StackProps {
+  env: cdk.Environment
   vpc: cdk.aws_ec2.IVpc
 }
 
 export class EksBuilder implements IEksBuilder {
+  public readonly stack!: cdk.Stack;
+
   // https://github.com/aws-quickstart/cdk-eks-blueprints/tree/main/examples
   constructor(scope: Construct, id: string, props: Props) {
+    const base = blueprints.EksBlueprint.builder()
+      .account(props.env.account)
+      .region(props.env.region)
+
+    const builder = () => base.clone();
   }
 }
--- src/sandbox/bin/sandbox.ts
+++ src/sandbox/bin/sandbox.ts
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import * as bp from './eks-blueprint';
 import { SandboxStack } from '../lib/sandbox-stack';
 import { config as DevProps } from '../env/develop';
 
 const app = new cdk.App();
 const environment = app.node.tryGetContext('environment');
 const props = getProperties(environment);
-const env = {
+const env: cdk.Environment = {
   account: props.account,
   region: props.region,
 };
 const vpc = new SandboxStack(app, 'SandboxStack', { env });
+new bp.EksBuilder(app, 'EksBuilder', { env, vpc: vpc.vpc });
 app.synth()
 
 cdk.Tags.of(app).add('owner', 'panicboat');
